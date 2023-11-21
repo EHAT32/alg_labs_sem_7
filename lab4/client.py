bee65749973ac49128ff935773d5e051fdfc579c
Roman <enatik14@gmail.com>
2023-11-21 15:54:18 +0300

    remothed isSignedUp method

--- lab4/client.py
+++ lab4/client.py
@@ -1,7 +1,4 @@
 class Client:
     def __init__(self, id : int) -> None:
         self.id = id
-        self.signedUp = False
-        
-    def isSignedUp(self) -> bool:
-        return self.signedUp
+        self.isSignedUp = False
