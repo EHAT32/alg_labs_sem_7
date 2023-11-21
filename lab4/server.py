56883d66a851cc9c451422be384ed2ffab4c7971
Roman <enatik14@gmail.com>
2023-11-21 15:51:10 +0300

    signUp list of clients

--- lab4/server.py
+++ lab4/server.py
@@ -3,12 +3,25 @@ from typing import List
 
 class Server:
     def __init__(self) -> None:
-        self.clients = []
+        self.clients = {}
         self.pool = []
         
     def addClient(self, client : Client) -> None:
-        self.clients.append(client)
+        if self.clients[client.id] is not None:
+            print('Client is already here!')
+            return
+        else:
+            self.clients[client.id] = client
         
     def addClients(self, clients : List[Client]) -> None:
         for client in clients:
             self.addClient(client)
+            
+    #activity
+    def signUpClient(self, ids : List[int]) -> None:
+        for clientId in ids:
+            client = self.clients[clientId]
+            if client.isSignedUp():
+                print(f'Client {clientId} is already signed up!')
+            else:
+                client.signedUp = True
